2c58c0ed3644e8db76ec74c6c13346439bc429f1
João Victor Monteiro <jvmsantos13@gmail.com>
2021-06-23 19:56:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function addValor () {
 
 function calcular () {
     
- document.querySelector('.resultado').innerHTML = 'Seu custo total de viagem é de R$ ' + ;
+ document.querySelector('.resultado').innerHTML = 'Seu custo total de viagem é de R$ ' + Var;
 }
