94b94f8c333096d20d5592784d796822495bb550
João Victor Monteiro <jvmsantos13@gmail.com>
2021-06-24 17:39:30 -0300

    Update

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function addValor () {
-
-}
-
-
 function calcular () {
-    
- document.querySelector('.resultado').innerHTML = 'Seu custo total de viagem é de R$ ' + Var;
+    document.addEventListener('click', function() {
+        var peru = document.querySelectorAll('input[valeu="780.00"][type="checkbox"]:checked').length;
+    })
+
+    document.addEventListener('click', function() {
+        var chile = document.querySelectorAll('input[valeu="960.00"][type="checkbox"]:checked').length;
+    })
+
+    document.addEventListener('click', function() {
+        var bolivia = document.querySelectorAll('input[valeu="870.00"][type="checkbox"]:checked').length;
+    })
+
+    document.addEventListener('click', function() {
+        var uruguai = document.querySelectorAll('input[valeu="910.00"][type="checkbox"]:checked').length;
+    })
+
+    document.addEventListener('click', function() {
+        var argentina = document.querySelectorAll('input[valeu="980.00"][type="checkbox"]:checked').length;
+    })
+
+    document.addEventListener('click', function() {
+        var voltar = document.querySelectorAll('input[valeu="350.00"][type="checkbox"]:checked').length;
+    })
 }
